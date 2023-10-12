3b9c8760fd97e306a7a182925903e61d561127bb
Aditya <mikukumarkeshri@gmail.com>
2023-10-12 21:52:07 +0530

    firstbar

---
+++ ADITYA/src/Length.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Length {
+	static int Length(char[]a){
+	int n=0;
+	for(char i:a) {
+		n++;
+	}
+	return n;
+}
+	public static void main(String[] args) {
+		System.out.println("Enter the string");
+		Scanner scan=new Scanner(System.in);
+		char[]a=scan.nextLine().toCharArray();
+		int n;
+		n=Length(a);
+		System.out.println("Length="+n);
+	}
+
+}
